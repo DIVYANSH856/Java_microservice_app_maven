a5729c8c3dfcfe661f97054ce7283fe3c50c07ad
DIVYANSH856 <74702000+DIVYANSH856@users.noreply.github.com>
2023-08-11 17:24:48 +0000

    containerized my application

---
+++ cri.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build backend image
+echo "Building backend image..."
+docker build -t module-name:1.0.0 ./backend
+
+# Build frontend1 image
+echo "Building frontend1 image..."
+docker build -t frontend:1.0.0 ./frontend
+
+# Build frontend2 image
+echo "Building frontend2 image..."
+docker build -t frontend1:1.0.0 ./frontend2
+
+echo "All images built successfully!"
+
